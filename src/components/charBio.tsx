b0d9fca413b0821114fd6c4b1e405984c6213c6a
Chris <chrisbrowne91@gmail.com>
2025-07-21 19:05:53 -0400

    remove name prop and add children prop

--- src/components/charBio.tsx
+++ src/components/charBio.tsx
@@ -1,21 +1,23 @@
 import { motion } from "framer-motion";
 import { Quote } from "./chatBubble";
-import { useState } from "react";
 
 export const Bio = ({
   imgUrl,
-  name,
+  visible,
   children,
 }: {
   imgUrl: string;
-  name: string;
+  visible: boolean;
   children: React.ReactNode;
 }) => {
-  const [visible, setVisible] = useState<boolean>(false);
-
   return (
     <motion.div className="flex flex-col justify-center items-center h-auto max-w-full min-h-screen py-12">
-      <Quote vis={visible} />
+      <Quote vis={visible}>
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia nisi
+        recusandae temporibus atque pariatur quam in iusto ea, harum ratione
+        dolor repellat alias blanditiis qui odit maxime ipsum quasi voluptatum.
+      </Quote>
       <div className="flex items-center flex-col lg:flex-row">
         <motion.img
           src={imgUrl}
@@ -26,32 +28,7 @@ export const Bio = ({
           transition={{ duration: 1, ease: "easeIn" }}
           viewport={{ once: true }}
         />
-        <div className="flex flex-col gap-3.5">
-          <motion.div
-            className="font-bold text-4xl underline italic lg:text-6xl overflow-hidden"
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true }}
-          >
-            {name.split("").map((l, i) => {
-              return (
-                <motion.span
-                  key={i}
-                  variants={{
-                    hidden: { opacity: 0, y: "-100%" },
-                    visible: { opacity: 1, y: 0 },
-                  }}
-                  transition={{ ease: "easeIn", duration: 1, delay: 0.05 * i }}
-                  className="inline-block underline"
-                >
-                  {l}
-                </motion.span>
-              );
-            })}
-          </motion.div>
-          <p className="font-bold text-2xl lg:text-3xl">{children}</p>
-          <button onClick={() => setVisible(!visible)}>Quote</button>
-        </div>
+        <p className="font-bold text-2xl lg:text-3xl hidden">{children}</p>
       </div>
     </motion.div>
   );
